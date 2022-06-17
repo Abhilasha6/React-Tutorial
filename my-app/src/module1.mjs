c395dfd6ca49e38031ae8e0f75b99e1439081067
Abhilasha Panda <81556030+Abhilasha6@users.noreply.github.com>
2022-06-17 12:14:55 +0530

    named and default export props

---
+++ my-app/src/module1.mjs
@@ -0,0 +1,11 @@
+const a="harry";
+const b="code";
+const c="react";
+const d="exporting default to module 2";
+
+export default d;
+
+//named export in js to module 2 file
+export{a};
+export{b};
+export{c};
---
+++ my-app/src/module2.mjs
@@ -0,0 +1,5 @@
+import pqr, {a, b, c} from '.myapp/src/module1.mjs'
+console.log(pqr);
+console.log(a);
+console.log(b);
+console.log(c);
