55a2640cde4e53261cb116be5442e4b6b869036b
Abhilasha Panda <81556030+Abhilasha6@users.noreply.github.com>
2022-06-19 00:09:49 +0530

    learning about states and useState hooks

--- my-app/src/Components/TextForm.js
+++ my-app/src/Components/TextForm.js
@@ -1,8 +1,16 @@
-import React from "react";
-
+import React,{useState} from "react";
+// state is equivalent to variable
 export default function TextForm(props) {
+
+  const[text, setText] = useState('Enter text here');
+  // setText("hellooo");
+// hooks help us use the properties of a class without using the class
+//we cannot update the value of a status as we update a normal variable, we have to always define a function to updat a state's value as mentioned in above statement
   return (
     <div>
+      { /* <h1>{props.heading} - {text}</h1> */}
+      <h1>{props.heading}</h1>
+
       <form>
         <div class="mb-3">
           {/* <label for="myBox" class="form-label">Email address</label> */}
@@ -12,10 +20,9 @@ export default function TextForm(props) {
             id="myBox"
             aria-describedby="emailHelp"
             rows="8"
+
+            value={text} //adds state to the variable
           />
-          <div id="emailHelp" class="form-text">
-            We'll never share your email with anyone else.
-          </div>
           <button className="btn btn-primary">Convert to uppercase</button>
         </div>
       </form>
