be2c32bd5941dc899f2f0b69602f655152ff5fb7
Abhilasha Panda <81556030+Abhilasha6@users.noreply.github.com>
2022-06-18 20:56:37 +0530

    added textform component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,16 @@
 // import logo from './logo.svg';
 import './App.css';
 import Navbar from './Components/Navbar';
+import TextForm from './Components/TextForm';
 
 function App() {
   return (
    <>
    <Navbar title = "TextUtils"/>
+   <div className='container'>
+   <TextForm/>
+   <TextForm heading="Enter the text to"></TextForm>
+   </div>
    </>
   );
 }
--- my-app/src/Components/TextForm.js
+++ my-app/src/Components/TextForm.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
 
-export default function TextForm() {
+export default function TextForm(props) {
   return (
     <div>
-      
+      <form>
+        <div class="mb-3">
+          {/* <label for="myBox" class="form-label">Email address</label> */}
+          <input
+            type="email"
+            class="form-control"
+            id="myBox"
+            aria-describedby="emailHelp"
+            rows="8"
+          />
+          <div id="emailHelp" class="form-text">
+            We'll never share your email with anyone else.
+          </div>
+          <button className="btn btn-primary">Convert to uppercase</button>
+        </div>
+      </form>
     </div>
-  )
+  );
 }
